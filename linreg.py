49e1bb461ba28c42281cfda3a6ad6ec5edf9839a
kmaster2520 <sathvik.kadaveru@gmail.com>
2017-05-18 20:18:10 -0400

    did knn

--- linreg.py
+++ linreg.py
@@ -2,11 +2,26 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
+
 
 style.use('fivethirtyeight')
 
-xs = np.array([1,2,3,4,5,6], dtype=np.float64)
-ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+#xs = np.array([1,2,3,4,5,6], dtype=np.float64)
+#ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+
+def create_dataset(hm, variance, step=2, correlation=False):
+    val = 1
+    ys = []
+    for i in range(hm):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val += step
+        elif correlation and correlation == 'neg':
+            val -= step
+    xs = [i for i in range(len(ys))]
+    return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
 
 '''
 y = mx+b solve for m and b:
@@ -16,7 +31,6 @@ b = Ey - m * Ex (y-intercept)
 
 SE = squared error
 r2 = r-squared = 1 - SE(y-hat) / SE(y-bar)
-
 
 
 '''
@@ -38,6 +52,10 @@ def coefficient_of_determination(ys_orig, ys_line):
     squared_error_regr = squared_error(ys_orig, ys_line)
     squared_error_y_mean = squared_error(ys_orig, y_mean_line)
     return 1.0 - (squared_error_regr / squared_error_y_mean)
+
+
+xs, ys = create_dataset(40, 40, 2, correlation='pos')
+
 
 m, b = best_fit_slope_and_intercept(xs, ys);
 #print(m,b)
